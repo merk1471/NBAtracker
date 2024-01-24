6eacdba5fddebd32b15bca116543fdd02e68808a
Mark <133434946+merk1471@users.noreply.github.com>
2024-01-24 14:29:35 -0500

    Delete blog directory

--- blog/client/blog-app/src/pages/LoginPage.jsx
+++
@@ -1,9 +0,0 @@
-export const LoginPage = () =>{
-    return (
-        <form action="">
-            <input type="text" placeholder="username"/>
-            <input type="text" placeholder="password"/>
-            <button>Login</button>
-        </form>
-    )
-}
--- blog/client/blog-app/src/pages/RegisterPage.jsx
+++
@@ -1,9 +0,0 @@
-export const RegisterPage = () =>{
-    return(
-        <form action="">
-            <input type="text" placeholder="username"/>
-            <input type="text" placeholder="password"/>
-            <button>Register</button>
-        </form>
-    )
-}
